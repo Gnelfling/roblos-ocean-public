5c50d67b61ec8844ba6caae1e8b0b6d2a7154e84
Gnelf <kleincolombo@gmail.com>
2025-11-24 17:47:24 -0300

    changed chunk size for better roleplaying experience or whatever type shit

--- ocean.lua
+++ ocean.lua
@@ -28,7 +28,7 @@ local function tween(obj, props, time, style, dir)
 end
 
 -- Configs (chunked ocean)
-local CHUNK_SIZE = 64               -- chunk width / length in studs
+local CHUNK_SIZE = 364              -- chunk width / length in studs
 local CHUNK_HEIGHT = 4              -- vertical thickness of water block (studs)
 local RENDER_DISTANCE_CHUNKS = 6    -- how many chunks radius around player to keep
 local CHUNK_CULL_PADDING = 2        -- extra padding before removing chunks
@@ -725,3 +725,4 @@ if transparencySlider and transparencySlider.Set then transparencySlider.Set(0.2
 if reflectionSlider and reflectionSlider.Set then reflectionSlider.Set(0.5) else reflectionSlider.OnGet = 0.5 end
 
 print("Ocean Chunked Generator loaded. Press M to toggle the menu. Press Generate to start streaming the ocean.")
+
